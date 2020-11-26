be0e977b61ecfa821adedbb4dc1129dad7f2bbcf
askhat01 <satekovaskhat@mail.ru>
2020-11-26 15:21:20 +0600

    add: e-mail and password validation

--- app/src/main/java/com/demo/userauthorizationapp/AuthorizationActivity.java
+++ app/src/main/java/com/demo/userauthorizationapp/AuthorizationActivity.java
@@ -1,11 +1,17 @@
 package com.demo.userauthorizationapp;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class AuthorizationActivity extends AppCompatActivity {
 
@@ -31,7 +37,11 @@ public class AuthorizationActivity extends AppCompatActivity {
     private View.OnClickListener mOnEnterClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            //TODO: обработка нажатия по кнопке
+            if (isLoginValid() && isPasswordValid()) {
+                // TODO: перейти в профиль пользователя (приложение)
+            } else {
+                showErrorMessage(R.string.error_login_input);
+            }
         }
     };
 
@@ -41,4 +51,16 @@ public class AuthorizationActivity extends AppCompatActivity {
             //TODO: обработка нажатия по кнопке
         }
     };
+
+    private boolean isLoginValid() {
+        return !TextUtils.isEmpty(mEtLogin.getText()) && Patterns.EMAIL_ADDRESS.matcher(mEtLogin.getText()).matches();
+    }
+
+    private boolean isPasswordValid() {
+        return !TextUtils.isEmpty(mEtPassword.getText());
+    }
+
+    private void showErrorMessage(@StringRes int string) {
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
+    }
 }
