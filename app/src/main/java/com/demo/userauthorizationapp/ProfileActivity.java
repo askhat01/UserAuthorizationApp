eea599dd0076a81951970defe4f1a20fe6ded0d3
askhat01 <satekovaskhat@mail.ru>
2020-11-26 16:21:10 +0600

    edit(activity): created fields, listeners in ProfileActivity for elements from its XML layout

---
+++ app/src/main/java/com/demo/userauthorizationapp/ProfileActivity.java
@@ -0,0 +1,34 @@
+package com.demo.userauthorizationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private AppCompatImageView mUserPhoto;
+    private EditText mUserLogin;
+    private EditText mUserPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        mUserPhoto = findViewById(R.id.image_user_photo);
+        mUserLogin = findViewById(R.id.edit_user_login);
+        mUserPassword = findViewById(R.id.edit_user_password);
+
+        mUserPhoto.setOnClickListener(mOnPhotoClickListener);
+    }
+
+    private View.OnClickListener mOnPhotoClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //TODO: обраотка нажатия на фото профиля
+        }
+    }
+}
